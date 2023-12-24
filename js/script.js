41bc9ddc24e565f1d8644fa7660b7ed7b4d989bd
EginRyzen <muhammadaliagil@gmail.com>
2023-12-24 20:33:38 +0700

    first-commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementBottom = reveals[i].getBoundingClientRect().bottom;
+
+        // Menambahkan atau menghapus kelas "active" berdasarkan kedalaman elemen dalam viewport
+        if (elementTop < windowHeight && elementBottom >= 0) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+
+
+// 1. tangkap element dengan class menu
+const menu = document.querySelector(".nav");
+
+// 2. jika element dengan class menu diklik
+menu.addEventListener('click', function (e) {
+    // 3. maka ambil element apa yang diklik oleh user
+    const targetMenu = e.target;
+
+    // 4. lalu cek, jika element itu adalah link dengan class menu__link
+    if (targetMenu.classList.contains('nav-link')) {
+
+        // 5. maka ambil menu link yang aktif
+        const menuLinkActive = document.querySelector("ul li a.active");
+
+        // 6. lalu cek, Jika menu link active ada dan menu yang di klik user adalah menu yang tidak sama dengan menu yang aktif, (cara cek-nya yaitu dengan membandingkan value attribute href-nya)
+        if (menuLinkActive !== null && targetMenu.getAttribute('href') !== menuLinkActive.getAttribute('href')) {
+
+            // 7. maka hapus class active pada menu yang sedang aktif
+            menuLinkActive.classList.remove('active');
+        }
+
+        // 8. terakhir tambahkan class active pada menu yang di klik oleh user
+        targetMenu.classList.add('active');
+    }
+});
+
+
